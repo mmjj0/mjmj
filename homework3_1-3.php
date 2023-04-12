cb2d0a74f03d17092c60210a26f374bc10a68473
mmjj0 <128334219+mmjj0@users.noreply.github.com>
2023-04-12 14:38:06 +0900

    Add files via upload

---
+++ homework3_1-3.php
@@ -0,0 +1,21 @@
+<?php
+    echo "1번<br/>";
+    $n = 30;
+    $sum=  0;
+    $prod = 1;
+    for($i = 1; $i <= $n; $i++) 
+    {
+        $sum += $i;
+        $prod *= $i;
+    }
+    echo "1부터 n까지의 숫자 합 = $sum"."입니다<br/>";
+    echo "1부터 n까지의 숫자 곱 = $prod"."입니다<br/>";
+?>
+
+<?php
+    echo "2번<br/>";
+?>
+
+<?php
+    echo "3번<br/>";
+?>
---
+++ homework3_4.php
@@ -0,0 +1,9 @@
+<?php
+    echo "4번<br/>";
+?>
+
+<html>
+<body>
+    직사각형_넓이 = <?php echo $_POST["직사각형_가로"]*$_POST["직사각형_세로"]; ?>
+</body>
+</html>
