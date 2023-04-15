8b1f76a327b6d97bfbbb5247e205352d9f31ad50
mmjj0 <128334219+mmjj0@users.noreply.github.com>
2023-04-15 13:11:25 +0900

    Update homework3_1-3.php

--- homework3_1-3.php
+++ homework3_1-3.php
@@ -1,5 +1,5 @@
 <?php
-    echo "1번<br/>";
+    echo "1번<br>";
     $n = 30;
     $sum=  0;
     $prod = 1;
@@ -8,12 +8,19 @@
         $sum += $i;
         $prod *= $i;
     }
-    echo "1부터 n까지의 숫자 합 = $sum"."입니다<br/>";
-    echo "1부터 n까지의 숫자 곱 = $prod"."입니다<br/>";
+    echo "1부터 n까지의 숫자 합 = $sum"."입니다<br>";
+    echo "1부터 n까지의 숫자 곱 = $prod"."입니다<br>";
 ?>
 
 <?php
-    echo "2번<br/>";
+    echo "2번<br>";
+    $n = 30;
+
+    for ($x = 0; $x < $n; $x++) {
+        $data[$x] = rand(0, 100);
+        echo "$data[$x] ";
+    }
+    echo "<br>";
 ?>
 
 <?php
