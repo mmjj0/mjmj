62df40fca4401d84027101ae464871c98c8c7342
mmjj0 <128334219+mmjj0@users.noreply.github.com>
2023-04-18 04:00:28 +0900

    Add files via upload

---
+++ homework3_5_form.php
@@ -0,0 +1,5 @@
+<form action = "homework3_5.php" method = "post">
+    년을 입력하세요 : <input type="number" name="y" /> <br/>
+    월을 입력하세요 : <input type="number" name="m" /> <br/>
+    <input type="submit" value="확인" />
+</form>
